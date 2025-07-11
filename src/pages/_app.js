2329798f8f497cab05babab4e1755ceddce6aeb4
sergiogonza <sergiogonzawr@gmail.com>
2025-07-11 12:07:35 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,59 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    // Google Analytics
+    const GA_ID = 'G-TYCLJJBRRJ';
+
+    const script1 = document.createElement('script');
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${GA_ID}');
+    `;
+    document.head.appendChild(script2);
+
+    const handleRouteChange = (url) => {
+      if (window.gtag) {
+        window.gtag('config', GA_ID, { page_path: url });
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
+  useEffect(() => {
+    // Facebook Pixel
+    const pixelId = '1883376389273067';
+
+    const script = document.createElement('script');
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+      n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '${pixelId}');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(script);
+
+    // Optional: Add <noscript> fallback manually in _document.js if needed
+  }, []);
+
+  return <Component {...pageProps} />;
 }
