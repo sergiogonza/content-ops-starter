6172da3814465b23cbcae7b307431b16858262b9
sergiogonza <sergiogonzawr@gmail.com>
2025-07-11 12:29:36 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,24 @@ import '../css/main.css';
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
+
+   useEffect(() => {
+    // Google Tag Manager (head)
+    const GTM_ID = 'GTM-MTGDMT9B';
+
+    const script = document.createElement('script');
+    script.innerHTML = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${GTM_ID}');
+    `;
+    document.head.appendChild(script);
+  }, []);
+  
+
+  
   useEffect(() => {
     // Google Analytics
     const GA_ID = 'G-TYCLJJBRRJ';
